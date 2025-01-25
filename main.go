0826fb439e04866960e8ca7d5a7a327fe5061c1b
RohanMishra315 <315scisyb2020rohanmishra@gmail.com>
2025-01-25 14:30:31 +0000

    Initial Commit

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 )
 
 func main() {
-	// Check wether kernel supports XDP or not
+	// Check whether kernel supports XDP or not
 	err := features.HaveProgramType(ebpf.XDP)
 	if errors.Is(err, ebpf.ErrNotSupported) {
 		fmt.Println("XDP program type is not supported")
@@ -35,22 +35,22 @@ func main() {
 	}
 	defer objs.Close()
 
-	// Execute `ip a` command for network interfaces and change this to an interface on your machine.
+	// Set interface to "lo" (loopback)
 	ifname := "lo"
 	iface, err := net.InterfaceByName(ifname)
 	if err != nil {
 		log.Fatalf("Getting interface %s: %s", ifname, err)
 	}
 
-	var port uint32 = 9254
+	var port uint32 = 4040
 
-	// put port number on which packets should be dropped.
+	// Put port number on which packets should be dropped.
 	err = objs.PortMap.Put(uint32(0), port)
 	if err != nil {
-		log.Printf("Unable to set port number default port will be used (default: 4040)\n %s", err)
+		log.Printf("Unable to set port number, default port will be used (default: 4040)\n%s", err)
 	}
 
-	// Attach count_packets to the network interface.
+	// Attach eBPF program to the loopback interface.
 	link, err := link.AttachXDP(link.XDPOptions{
 		Program:   objs.DropPackets,
 		Interface: iface.Index,
